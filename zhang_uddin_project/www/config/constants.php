0d75878583edee021a98271bee0c881ee2f4aa06
Zhang <tzhang@epmg.com>
2019-06-29 18:41:31 -0400

    initial commit for vanguard project

---
+++ zhang_uddin_project/www/config/constants.php
@@ -0,0 +1,13 @@
+<?php
+
+//define('EMAIL', 'keevee810@gmail.com');
+//define('EMAIL', 'ga6198@wayne.edu');
+define('EMAIL', 'kylenlulavel@gmail.com');
+define('PASSWORD', 'bolter82');
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'john');
+define('DB_PASS', 'pass1234');
+define('DB_NAME', 'vanguard');
+?>
