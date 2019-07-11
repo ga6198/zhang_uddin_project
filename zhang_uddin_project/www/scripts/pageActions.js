a6f921c0d3a734b3e5a70086e0745f19d6cbb6fa
Zhang <tzhang@epmg.com>
2019-07-11 16:28:02 -0400

    profile images on decks and links in menu

---
+++ zhang_uddin_project/www/scripts/pageActions.js
@@ -0,0 +1,10 @@
+﻿//command for profile link
+function goToProfile() {
+    var creator_id = sessionStorage.getItem("user_id");
+    var creator = sessionStorage.getItem("username");
+    var userinfo = { creator: creator, creator_id: creator_id };
+
+    //save user info into session variables
+    sessionStorage.setItem('userinfo', JSON.stringify(userinfo));
+    location.href = "profile.html";
+}
