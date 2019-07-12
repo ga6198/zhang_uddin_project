ae578936a1569885ae94f06990747e5c41b8abab
Zhang <tzhang@epmg.com>
2019-07-11 20:13:23 -0400

    message changes to ip

--- zhang_uddin_project/www/scripts/platform.js
+++ zhang_uddin_project/www/scripts/platform.js
@@ -1,5 +1,6 @@
 ﻿var platform;
 
+/*
 if (navigator.platform.match(/Win32|MacIntel/)) {
     platform = "http://127.0.0.1/";
 }
@@ -12,3 +13,26 @@ else {
     //platform = "http://192.168.1.71/";
     platform = "http://10.10.10.110/";
 }
+*/
+
+//set ip address of server
+function setPlatformId() {
+    //var ip = document.getElementById("ip-box").innerHTML;
+    var ip = $("#ip-box").val();
+
+    //alert(ip);
+
+    if (navigator.platform.match(/Win32|MacIntel/)) {
+        platform = "http://127.0.0.1/";
+    }
+    else {
+        platform = "http://" + ip + "/";
+    }
+
+    //set ip into session
+    sessionStorage.setItem("platform", platform);
+
+    document.getElementById("ip-info").innerHTML = "IP set as: " + platform;
+}
+
+platform = sessionStorage.getItem("platform");
